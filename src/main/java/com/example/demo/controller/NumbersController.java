c6c79b5e90fbbe2eb03b4a1b3fc4cd6f6a152141
wi22b050 <wi22b050@technikum-wien.at>
2023-06-28 16:26:33 +0200

    Add Feature commited and pushed.

---
+++ src/main/java/com/example/demo/controller/NumbersController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.NumbersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
+public class NumbersController {
+
+    private final NumbersService numbersService;
+
+    @Autowired
+    private NumbersController(NumbersService numbersService){
+        this.numbersService = numbersService;
+    }
+
+}
