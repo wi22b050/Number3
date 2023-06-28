8b98ec21116e96e93c428bf15a56490f084f2d84
wi22b050 <wi22b050@technikum-wien.at>
2023-06-28 16:44:58 +0200

    Added a feature that divides two integers.

--- src/main/java/com/example/demo/controller/NumbersController.java
+++ src/main/java/com/example/demo/controller/NumbersController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.service.NumbersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
